9d1b7e8e38c0e402ad09bc85c114ea8985f1bacb
S-Onuki <k022g0020@g.neec.ac.jp>
2023-06-07 14:44:44 +0900

    04_04完了

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -31,7 +31,10 @@ PixelShaderOutput main(VertexShaderOutput input)
     
     if (gMaterial.enableLighting != 0)
     {
-        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        //float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        //output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.f);
         output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
     }
     else
