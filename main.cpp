3aece5b51492fcea7242a15b8fa241b4a69a0205
S-Onuki <k022g0020@g.neec.ac.jp>
2023-07-12 03:55:38 +0900

    微調整

--- main.cpp
+++ main.cpp
@@ -55,14 +55,12 @@ struct DirectResourceLeakChecker {
 
 #pragma region Report Live Objects
 
-
 		// リソースリークチェック
 		Microsoft::WRL::ComPtr<IDXGIDebug1> debug;
 		if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(debug.GetAddressOf())))) {
 			debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
 			debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
 			debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
-			//debug->Release();
 		}
 
 #pragma endregion
