d50cd36c96e53e77c013da895bca7602567678a0
S-Onuki <k022g0020@g.neec.ac.jp>
2023-08-09 18:58:16 +0900

    ゲームパッド取得

--- DirectBase/Input/Input.h
+++ DirectBase/Input/Input.h
@@ -6,8 +6,6 @@
 #pragma comment(lib, "dinput8.lib")
 #pragma comment(lib, "dxguid.lib")
 
-#include <wrl.h>
-
 #include <array>
 #include <stdint.h>
 class Input
@@ -18,6 +16,32 @@ public:
 
 private:
 
+};
+
+
+enum class KeyPad {
+	DPAD_UP = 0x0001,
+	DPAD_DOWN = 0x0002,
+	DPAD_LEFT = 0x0004,
+	DPAD_RIGHT = 0x0008,
+	START = 0x0010,
+	BACK = 0x0020,
+	LEFT_THUMB = 0x0040,
+	RIGHT_THUMB = 0x0080,
+	LEFT_SHOULDER = 0x0100,
+	RIGHT_SHOULDER = 0x0200,
+	A = 0x1000,
+	B = 0x2000,
+	X = 0x4000,
+	Y = 0x8000,
+};
+
+struct VirtualPad {
+	Vector2 stickR_;
+	Vector2 stickL_;
+	WORD button_;
+	float triggerR_;
+	float triggerL_;
 };
 
 class DirectInput
@@ -52,5 +76,18 @@ public:
 	bool IsRelease(const uint8_t key) const {
 		return key_[!inputTarget][key] && !key_[inputTarget][key];
 	}
+
+};
+class XInput
+{
+	XInput() = default;
+	XInput(const XInput &) = delete;
+	XInput operator=(const XInput &) = delete;
+	~XInput() = default;
+
+public:
+
+
+private:
 
 };
