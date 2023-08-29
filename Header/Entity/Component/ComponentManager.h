e82b2ffc495dffa47000ba184802831174ca5b80
S-Onuki <k022g0020@g.neec.ac.jp>
2023-08-30 03:33:15 +0900

    動作確認

---
+++ Header/Entity/Component/ComponentManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+#include "../Object.h"
+
+class ComponentManager
+{
+	ComponentManager() = default;
+	ComponentManager(const ComponentManager &) = delete;
+	ComponentManager &operator=(const ComponentManager &) = delete;
+
+	~ComponentManager() = default;
+public:
+
+	template<typename T>
+	void Register();
+
+private:
+	std::unordered_map<std::string, IComponent *(*)(Object *const object)> componentMap_;
+};
+
+template<typename T>
+inline void ComponentManager::Register() {
+	static_assert(std::is_base_of<IComponent, T>::value, "テンプレート型はIComponentクラスの派生クラスではありません");
+	componentMap_[typeid(T).name] = T(Object *const);
+}
