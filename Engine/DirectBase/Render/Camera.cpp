01ff7fffbff697d91ca0144bbd6d1966f3a24810
S-Onuki <k022g0020@g.neec.ac.jp>
2023-10-03 00:26:55 +0900

    カメラをテンプレートで分解

--- Engine/DirectBase/Render/Camera.cpp
+++ Engine/DirectBase/Render/Camera.cpp
@@ -4,52 +4,78 @@
 #include "../Base/DirectXCommon.h"
 
 #include "../Render/Render.h"
-#include "../Base/WinApp.h"
 
 
-//template<>
-//void Camera<Render::CameraType::Projecction>::CalcMatrix() {
-//	matView_ = Matrix4x4::Affine(Vector3::one, rotation_, translation_).InverseSRT();
-//	matProjection_ = Render::MakePerspectiveFovMatrix(fovAngleY, aspectRatio, nearZ, farZ);
-//}
+void Camera<Render::CameraType::Projecction>::Init() {
+	UpdateMatrix();
+}
 
-//template<>
-//void Camera<Render::CameraType::Othographic>::CalcMatrix() {
-//	matView_ = Matrix4x4::Affine(Vector3::one, rotation_, translation_).InverseSRT();
-//	Vector2 windowSize = { (float)WinApp::kWindowWidth,(float)WinApp::kWindowHeight };
-//	matProjection_ = Render::MakeOrthographicMatrix(windowSize * -.5f, windowSize * +.5f, 0.f, 100.f);
-//}
+void Camera<Render::CameraType::Projecction>::UpdateMatrix() {
+	CalcMatrix();
+	TransferMatrix();
+}
 
-//template<>
-//void Camera<Render::CameraType::Projecction>::Init() {
-//	UpdateMatrix();
-//}
+void Camera<Render::CameraType::Projecction>::TransferMatrix() {
+	constData_->view = matView_;
+	constData_->projection = matProjection_;
+	constData_->cameraPos = translation_;
+}
 
-//template<>
-//void Camera<Render::CameraType::Projecction>::UpdateMatrix() {
-//	CalcMatrix();
-//	TransferMatrix();
-//}
+bool Camera<Render::CameraType::Projecction>::ImGuiWidget() {
 
-//template<>
-//void Camera<Render::CameraType::Projecction>::TransferMatrix() {
-//	constData_->view = matView_;
-//	constData_->projection = matProjection_;
-//	constData_->cameraPos = translation_;
-//}
+	if (ImGui::TreeNode("Camera")) {
+		bool isUsing = false;
 
-//template<>
-//void Camera<Render::CameraType::Projecction>::ImGuiWidget() {
-//
-//	if (ImGui::TreeNode("Camera")) {
-//		bool isUsing = false;
-//
-//		isUsing |= ImGui::DragFloat3("Rotate", &rotation_.x, Angle::Dig2Rad);
-//
-//		isUsing |= ImGui::DragFloat3("Transform", &translation_.x, 0.01f);
-//
-//		ImGui::TreePop();
-//		return isUsing;
-//	}
-//	return false;
-//}
+		isUsing |= ImGui::DragFloat3("Rotate", &rotation_.x, Angle::Dig2Rad);
+
+		isUsing |= ImGui::DragFloat3("Transform", &translation_.x, 0.01f);
+
+		ImGui::TreePop();
+		return isUsing;
+	}
+	return false;
+}
+
+void Camera<Render::CameraType::Projecction>::CalcMatrix() {
+	matView_ = Matrix4x4::Affine(Vector3::one, rotation_, translation_).InverseSRT();
+	matProjection_ = Render::MakePerspectiveFovMatrix(fovAngleY, aspectRatio, nearZ, farZ);
+}
+
+
+
+void Camera<Render::CameraType::Othographic>::Init() {
+	UpdateMatrix();
+}
+
+void Camera<Render::CameraType::Othographic>::UpdateMatrix() {
+	CalcMatrix();
+	TransferMatrix();
+}
+
+void Camera<Render::CameraType::Othographic>::TransferMatrix() {
+	constData_->view = matView_;
+	constData_->projection = matProjection_;
+	constData_->cameraPos = translation_;
+}
+
+bool Camera<Render::CameraType::Othographic>::ImGuiWidget() {
+
+	if (ImGui::TreeNode("Camera")) {
+		bool isUsing = false;
+
+		isUsing |= ImGui::DragFloat("Rotate", &rotation_.z, Angle::Dig2Rad);
+
+		isUsing |= ImGui::DragFloat2("Transform", &translation_.x, 0.01f);
+
+		ImGui::TreePop();
+		return isUsing;
+	}
+	return false;
+}
+
+
+void Camera<Render::CameraType::Othographic>::CalcMatrix() {
+	matView_ = Matrix4x4::Affine(Vector3::one, rotation_, translation_).InverseSRT();
+
+	matProjection_ = Render::MakeOrthographicMatrix(windowSize_ * -.5f, windowSize_ * +.5f, 0.f, 100.f);
+}
--- Engine/DirectBase/Render/Camera.h
+++ Engine/DirectBase/Render/Camera.h
@@ -11,6 +11,8 @@
 #include "../../../Utils/Math/Math.hpp"
 #include "../Base/CBuffer.h"
 
+#include "../Base/WinApp.h"
+
 template <Render::CameraType T>
 class Camera;
 
@@ -53,105 +55,65 @@ public:
 	Matrix4x4 matView_{};
 	Matrix4x4 matProjection_{};
 
-	void Init() { UpdateMatrix(); }
+	void Init();
 
-	void CalcMatrix() {
-		matView_ = Matrix4x4::Affine(Vector3::one, rotation_, translation_).InverseSRT();
-		matProjection_ = Render::MakePerspectiveFovMatrix(fovAngleY, aspectRatio, nearZ, farZ);
-	}
+	void CalcMatrix();
 
-	void UpdateMatrix() {
-		CalcMatrix();
-		TransferMatrix();
-	}
+	void UpdateMatrix();
 
-	void TransferMatrix() {
-		constData_->view = matView_;
-		constData_->projection = matProjection_;
-		constData_->cameraPos = translation_;
-	}
+	void TransferMatrix();
 
-	bool ImGuiWidget() {
-
-		if (ImGui::TreeNode("Camera")) {
-			bool isUsing = false;
-
-			isUsing |= ImGui::DragFloat3("Rotate", &rotation_.x, Angle::Dig2Rad);
-
-			isUsing |= ImGui::DragFloat3("Transform", &translation_.x, 0.01f);
-
-			ImGui::TreePop();
-			return isUsing;
-		}
-		return false;
-	}
+	bool ImGuiWidget();
 
 
 };
-//
-///// @brief 正射影投影カメラ
-//template<>
-//class Camera<Render::CameraType::Othographic> {
-//	template<class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
-//
-//public:
-//
-//	struct CameraMatrix {
-//		Matrix4x4 view;
-//		Matrix4x4 projection;
-//		Vector3 cameraPos;
-//	};
-//
-//	Camera() = default;
-//	~Camera() = default;
-//
-//	CBuffer<CameraMatrix> constData_;
-//
-//#pragma region ビュー行列の設定
-//
-//	Vector3 rotation_ = { 0, 0, 0 };
-//	Vector3 translation_ = { 0, 0, -5 };
-//
-//#pragma endregion
-//
-//#pragma region 射影行列の設定
-//	// 垂直方向視野角
-//	float fovAngleY = 45.0f * Angle::Dig2Rad;
-//	// ビューポートのアスペクト比
-//	float aspectRatio = 16.f / 9;
-//	// 深度限界（手前側）
-//	float nearZ = 0.1f;
-//	// 深度限界（奥側）
-//	float farZ = 1000.0f;
-//#pragma endregion
-//
-//	Matrix4x4 matView_{};
-//	Matrix4x4 matProjection_{};
-//
-//	void Init();
-//
-//	void CalcMatrix();
-//
-//	/// @brief 前方宣言
-//	/// @tparam T カメラタイプ
-//	template<Render::CameraType T>
-//	void CalcMatrix();
-//
-//	/// @brief 透視投影計算
-//	template<>
-//	void CalcMatrix<Render::CameraType::Projecction>();
-//
-//	/// @brief 正射影計算
-//	template<>
-//	void CalcMatrix<Render::CameraType::Othographic>();
-//
-//	void UpdateMatrix();
-//
-//	template<Render::CameraType T>
-//	void UpdateMatrix();
-//
-//	void TransferMatrix();
-//
-//	bool ImGuiWidget();
-//
-//};
+
+/// @brief 正射影投影カメラ
+template<>
+class Camera<Render::CameraType::Othographic> {
+	template<class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+public:
+
+	struct CameraMatrix {
+		Matrix4x4 view;
+		Matrix4x4 projection;
+		Vector3 cameraPos;
+	};
+
+	Camera() = default;
+	~Camera() = default;
+
+	CBuffer<CameraMatrix> constData_;
+
+#pragma region ビュー行列の設定
+
+	Vector3 rotation_ = { 0, 0, 0 };
+	Vector3 translation_ = { 0, 0, -5 };
+
+#pragma endregion
+
+#pragma region 射影行列の設定
+
+	// 画面サイズ
+	Vector2 windowSize_ = { (float)WinApp::kWindowWidth,(float)WinApp::kWindowHeight };
+	// 深度限界（手前側）
+	float nearZ = 0.f;
+	// 深度限界（奥側）
+	float farZ = 100.0f;
+#pragma endregion
+
+	Matrix4x4 matView_{};
+	Matrix4x4 matProjection_{};
+
+	void Init();
+
+	void CalcMatrix();
+
+	void UpdateMatrix();
+
+	void TransferMatrix();
+
+	bool ImGuiWidget();
+
+};
