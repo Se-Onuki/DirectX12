c2f0576ad22de1a2c6dcdfc565f5eaadaed9d09d
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2023-11-16 10:07:19 +0900

    プッシュ

---
+++ Header/Object/Particle/BaseParticle.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "../Particle.h"
+#include "../../../Engine/DirectBase/Base/CBuffer.h"
+
+/// <summary>
+/// パーティクル基底クラス
+/// </summary>
+class BaseParticle
+{
+private: // メンバ変数
+
+	<Particle::ParticleData>
+
+};
+
