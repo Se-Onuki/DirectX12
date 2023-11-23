9b79b356534f4e155bbba4c4f0fccf03481596b8
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2023-11-23 13:45:53 +0900

    LoadScene内であらかたのモデルやテクスチャを読み込む

--- Scene/LoadScene.cpp
+++ Scene/LoadScene.cpp
@@ -18,13 +18,12 @@ void LoadScene::OnEnter()
 	textureManager_ = TextureManager::GetInstance(); // テクスチャ
 	modelManager_ = ModelManager::GetInstance();	 // モデル
 
-	// モデルの読み込み
+	// 3Dモデルの読み込み
 	modelManager_->CreateDefaultModel(); // デフォルトモデルの読み込み
 	modelManager_->AddModel("Box", Model::LoadObjFile("", "box.obj"));
 	modelManager_->AddModel("RedBox", Model::LoadObjFile("", "box.obj"))->materialMap_["Material"]->materialBuff_->color = Vector4{ 1.f,0.f,0.f,1.f };
-	modelManager_->AddModel("GrassModel", Model::LoadObjFile("Model/Ground/", "Ground.obj"))->materialMap_["Material"]->texHandle_ = textureManager_->Load("Model/Ground/GrassTex.png");
-	modelManager_->AddModel("DirtModel", Model::LoadObjFile("Model/Ground/", "Ground.obj"))->materialMap_["Material"]->texHandle_ = textureManager_->Load("Model/Ground/DustTex.png");
-
+	modelManager_->AddModel("GrassModel", Model::LoadObjFile("Model/Ground/", "Ground.obj")); // 草ブロック
+	modelManager_->AddModel("DirtModel", Model::LoadObjFile("Model/Ground/", "Ground.obj"))->materialMap_.begin()->second->texHandle_ = TextureManager::Load("Model/Ground/DustTex.png"); // 土ブロック
 	modelManager_->AddModel("PlayerBody", Model::LoadObjFile("Model/PlayerModel/Body/", "Body.obj")); // プレイヤーの体
 	modelManager_->AddModel("PlayerEye", Model::LoadObjFile("Model/PlayerModel/Eye/", "Eye.obj")); // プレイヤーの瞳
 	modelManager_->AddModel("PlayerHelmet", Model::LoadObjFile("Model/PlayerModel/Helmet/", "Helmet.obj")); // プレイヤーのヘルメット
@@ -33,6 +32,16 @@ void LoadScene::OnEnter()
 	modelManager_->AddModel("PlayerArm_R", Model::LoadObjFile("Model/PlayerModel/CharaArm/", "Arm_R.obj")); // プレイヤーの右腕
 	modelManager_->AddModel("PlayerFoot_L", Model::LoadObjFile("Model/PlayerModel/Foot/", "Foot_L.obj")); // プレイヤーの左足
 	modelManager_->AddModel("PlayerFoot_R", Model::LoadObjFile("Model/PlayerModel/Foot/", "Foot_R.obj")); // プレイヤーの右足
+
+	/// UI関係読み込み
+
+	// ステージ選択画面
+	TextureManager::Load("UI/TD2_3week_2/StageSelect/StageSelectUI.png");		   // ステージ選択画面
+	TextureManager::Load("UI/TD2_3week_2/StageSelect/StageSelectCursorLeft.png");  // ステージ選択画面選択矢印_左
+	TextureManager::Load("UI/TD2_3week_2/StageSelect/StageSelectCursorRight.png"); // ステージ選択画面_右
+	// ゲーム画面
+	TextureManager::Load("UI/TD2_3week_2/InGame/ingameStopUI.png");		// 一時停止画面UI
+	TextureManager::Load("UI/TD2_3week_2/InGame/ingameStopCursor.png"); // 停止カーソル
 }
 
 void LoadScene::OnExit()
