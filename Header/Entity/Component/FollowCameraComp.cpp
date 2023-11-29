1eadf57fa603418edd03740e7dacd9506731d151
S-Onuki <k022g0020@g.neec.ac.jp>
2023-11-30 03:24:26 +0900

    カメラの上下を実装

--- Header/Entity/Component/FollowCameraComp.cpp
+++ Header/Entity/Component/FollowCameraComp.cpp
@@ -12,8 +12,20 @@ void FollowCameraComp::Update() {
 
 	Vector3 linePoint = SoLib::Lerp(line_.ClosestPoint(pTarget_->GetGrobalPos()), prePos_, vLerpValue);
 
-	camera_->translation_ = offset_.GetItem() * Matrix4x4::EulerRotate(rotate_) + SoLib::Lerp(pTarget_->GetGrobalPos(), linePoint, vLerpValue) + addOffset_;
-	camera_->translation_.y = offset_.GetItem().y + SoLib::Lerp(pTarget_->GetGrobalPos().y, linePoint.y, 0.25f) + addOffset_->y;
+
+	if (rotate_.x > 15._deg) {
+		rotate_.x = SoLib::Lerp(rotate_.x, 15._deg, 0.2f);
+	}
+	else if (rotate_.x < -40._deg) {
+		rotate_.x = SoLib::Lerp(rotate_.x, -40._deg, 0.2f);
+	}
+
+	SoLib::ImGuiText("FollowCameraAngle", SoLib::to_string(rotate_));
+
+	const Vector3 cameraOffset = offset_.GetItem() * Matrix4x4::EulerRotate(rotate_);
+
+	camera_->translation_ = cameraOffset + SoLib::Lerp(pTarget_->GetGrobalPos(), linePoint, vLerpValue);
+	camera_->translation_.y = cameraOffset.y + SoLib::Lerp(pTarget_->GetGrobalPos().y, linePoint.y, 0.25f) + addOffset_->y;
 
 	Vector3 facing = linePoint - camera_->translation_;
 
@@ -34,8 +46,9 @@ void FollowCameraComp::AddRotate(const Vector3 &euler) {
 	if (euler.LengthSQ() != 0.f) {
 		if (CameraManager::GetInstance()->GetUseCamera() == camera_) {
 			rotate_ += euler;
+
 			camera_->rotation_ += euler;
-			camera_->UpdateMatrix();
+			camera_->CalcMatrix();
 		}
 	}
 }
