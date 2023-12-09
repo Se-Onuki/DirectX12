1fcefed10bbe77590257e20df0c07da3aba37551
S-Onuki <k022g0020@g.neec.ac.jp>
2023-12-09 21:40:30 +0900

    記述のルールの修正

--- Engine/DirectBase/Base/Audio.h
+++ Engine/DirectBase/Base/Audio.h
@@ -13,8 +13,8 @@
 
 class Audio {
 	Audio() = default;
-	Audio(const Audio&) = delete;
-	Audio operator=(const Audio&) = delete;
+	Audio(const Audio &) = delete;
+	Audio operator=(const Audio &) = delete;
 	~Audio() = default;
 
 public:
@@ -41,7 +41,7 @@ public:
 		// 波形フォーマット
 		WAVEFORMATEX wfex;
 		// バッファの先頭アドレス
-		BYTE* pBuffer;
+		BYTE *pBuffer;
 		// バッファのサイズ
 		uint32_t bufferSize;
 
@@ -51,7 +51,7 @@ public:
 	// 再生データ
 	struct Voice {
 		uint32_t handle = 0u;
-		IXAudio2SourceVoice* sourceVoice = nullptr;
+		IXAudio2SourceVoice *sourceVoice = nullptr;
 	};
 
 	/// <summary>
@@ -66,17 +66,17 @@ public:
 		// バッファストリームの再生が終了した時
 		STDMETHOD_(void, OnStreamEnd)(THIS) {};
 		// バッファの使用開始時
-		STDMETHOD_(void, OnBufferStart)(THIS_ void*) {};
+		STDMETHOD_(void, OnBufferStart)(THIS_ void *) {};
 		// バッファの末尾に達した時
-		STDMETHOD_(void, OnBufferEnd)(THIS_ void* pBufferContext);
+		STDMETHOD_(void, OnBufferEnd)(THIS_ void *pBufferContext);
 		// 再生がループ位置に達した時
-		STDMETHOD_(void, OnLoopEnd)(THIS_ void*) {};
+		STDMETHOD_(void, OnLoopEnd)(THIS_ void *) {};
 		// ボイスの実行エラー時
-		STDMETHOD_(void, OnVoiceError)(THIS_ void*, HRESULT) {};
+		STDMETHOD_(void, OnVoiceError)(THIS_ void *, HRESULT) {};
 	};
 
 
-	static Audio* const GetInstance() {
+	static Audio *const GetInstance() {
 		static Audio instatnce{};
 		return &instatnce;
 	}
@@ -84,22 +84,22 @@ public:
 	void Finalize();
 
 	void StaticInit();
-	void PlayWave(const SoundData& soundData, bool loopFlag, float volume);
+	void PlayWave(const SoundData &soundData, bool loopFlag, float volume);
 	uint32_t PlayWave(uint32_t index, bool loopFlag, float volume);
 
 	bool IsPlaying(uint32_t voiceHandle);
 
 	void StopWave(uint32_t voiceHandle);
 	void StopAllWave();
-	uint32_t LoadWave(const char* filename);
-	SoundData* const GetWave(const uint32_t index);
+	uint32_t LoadWave(const char *filename);
+	SoundData *const GetWave(const uint32_t index);
 private:
 
 	Microsoft::WRL::ComPtr<IXAudio2> xAudio2_ = nullptr;
-	IXAudio2MasteringVoice* masterVoice_ = nullptr;
+	IXAudio2MasteringVoice *masterVoice_ = nullptr;
 
 	std::array<std::unique_ptr<SoundData>, 512u> soundArray_;
-	std::set<Voice*> voices_;
+	std::set<Voice *> voices_;
 	// 次に使う再生中データの番号
 	uint32_t indexVoice_ = 0u;
 	// オーディオコールバック
@@ -107,4 +107,4 @@ private:
 
 };
 
-Audio::SoundData SoundLoadWave(const char* filename);
+Audio::SoundData SoundLoadWave(const char *filename);
