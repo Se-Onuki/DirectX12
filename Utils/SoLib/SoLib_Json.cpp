458e83a86618c357278a35e053bf42629565c82d
S-Onuki <k022g0020@g.neec.ac.jp>
2023-12-11 10:10:58 +0900

    ヘッダ側に移動

--- Utils/SoLib/SoLib_Json.cpp
+++ Utils/SoLib/SoLib_Json.cpp
@@ -1,5 +1,4 @@
 #include "SoLib_Json.h"
-#include <variant>
 
 void from_json(const nlohmann::json &json, Vector2 &data) {
 	if (json.is_array() && json.size() == SoLib::Traits<Vector2>::Size) {
@@ -27,93 +26,10 @@ void from_json(const nlohmann::json &json, Vector4 &data) {
 		}
 	}
 }
-
-template<SoLib::IsNotPointer T>
-using JsonItemPtr = std::variant<int32_t T:: *, float T:: *, std::string T:: *, Vector2 T:: *, Vector3 T:: *, Vector4 T:: *, Quaternion T:: *>;
-
-template<SoLib::IsNotPointer T>
-struct JsonPairStruct {
-	enum class TypeName {
-		kChar,
-		kInt,
-		kFloat,
-	};
-
-	std::string key;
-	std::list<JsonItemPtr<T>> ptrList;
-	TypeName typeName = TypeName::kFloat;
-};
-
-template<SoLib::IsNotPointer T>
-using JsonPair = std::list<JsonPairStruct<T>>;
-
-template<SoLib::IsNotPointer T>
-JsonPair<T> GetJsonPair();
-
-template<>
-JsonPair<Quaternion> GetJsonPair<Quaternion>() {
-	static JsonPair<Quaternion> result;
-	if (not result.size()) {
-		result.push_back(JsonPairStruct<Quaternion>{ "vec", { reinterpret_cast<Vector3 Quaternion:: *>(&Quaternion::x) }});
-		result.push_back(JsonPairStruct<Quaternion>{ "w", { &Quaternion::w } });
-	}
-	return result;
-}
-
 void to_json(nlohmann::json &json, const Quaternion &data) {
-	const auto &jsonPair = GetJsonPair<Quaternion>();
-
-	for (const auto &pairData : jsonPair) {
-		const auto &key = pairData.key;
-		const auto &ptrList = pairData.ptrList;
-		if (ptrList.size() == 1u) {
-
-			auto &jsonItem = json[key];
-			std::visit([&jsonItem, &data](auto Quaternion:: *const ptrv)
-				{
-					jsonItem = data.*ptrv;
-				}, ptrList.front());
-		}
-		else {
-			for (const auto &member : ptrList) {
-
-				auto &jsonItem = json[key];
-				std::visit([&jsonItem, &data](auto Quaternion:: *const ptrv)
-					{
-						jsonItem.push_back(data.*ptrv);
-					}, member);
-
-			}
-		}
-
-	}
+	SoLib::ToJson(json, data);
 }
 
 void from_json(const nlohmann::json &json, Quaternion &data) {
-	const auto &jsonPair = GetJsonPair<Quaternion>();
-
-	for (const auto &pairData : jsonPair) {
-		const auto &key = pairData.key;
-		const auto &ptrList = pairData.ptrList;
-		if (ptrList.size() == 1u) {
-
-			const auto &jsonItem = json.at(key);
-			std::visit([&jsonItem, &data](auto Quaternion:: *const ptrv)
-				{
-					data.*ptrv = jsonItem;
-				}, ptrList.front());
-
-		}
-		else {
-			uint32_t index = 0u;
-			for (const auto &member : ptrList) {
-				const auto &jsonItem = json.at(key).at(index);
-				std::visit([&jsonItem, &data](auto Quaternion:: *const ptrv)
-					{
-						data.*ptrv = jsonItem;
-					}, member);
-				index++;
-			}
-		}
-	}
+	SoLib::FromJson(json, data);
 }
--- Utils/SoLib/SoLib_Json.h
+++ Utils/SoLib/SoLib_Json.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "SoLib_Traits.h"
+#include <variant>
+
 #include "../../externals/nlohmann/json.hpp"
 
 #include "../Math/Matrix2x2.h"
@@ -10,10 +12,101 @@
 #include "../Math/Vector3.h"
 #include "../Math/Vector4.h"
 
+namespace SoLib {
+
+	template<SoLib::IsNotPointer T>
+	using JsonItemPtr = std::variant<bool T:: *, int32_t T:: *, float T:: *, std::string T:: *, Vector2 T:: *, Vector3 T:: *, Vector4 T:: *, Quaternion T:: * >;
+
+	template<SoLib::IsNotPointer T>
+	using JsonPair = std::list<std::pair<std::string, std::list<JsonItemPtr<T>>>>;
+
+	template<SoLib::IsNotPointer T>
+	inline JsonPair<T> GetJsonPair();
+
+	template<>
+	inline JsonPair<Quaternion> GetJsonPair<Quaternion>() {
+		JsonPair<Quaternion> result;
+		result.push_back({ "vec", { reinterpret_cast<Vector3 Quaternion:: *>(&Quaternion::x) } });
+		result.push_back({ "w", { &Quaternion::w } });
+		return result;
+	}
+
+
+	template<SoLib::IsNotPointer T>
+	inline void ToJson(nlohmann::json &json, const T &data);
+
+	template<SoLib::IsNotPointer T>
+	inline void FromJson(const nlohmann::json &json, T &data);
+
+}
+
 void from_json(const nlohmann::json &json, Vector2 &data);
 void from_json(const nlohmann::json &json, Vector3 &data);
 void from_json(const nlohmann::json &json, Vector4 &data);
 
-
-void to_json(nlohmann::json &json, const  Quaternion &data);
+void to_json(nlohmann::json &json, const Quaternion &data);
 void from_json(const nlohmann::json &json, Quaternion &data);
+
+#pragma region Func
+
+namespace SoLib {
+
+	template<SoLib::IsNotPointer T>
+	inline void ToJson(nlohmann::json &json, const T &data) {
+		static const auto &jsonPair = GetJsonPair<T>();
+
+		for (const auto &[key, ptrList] : jsonPair) {
+			if (ptrList.size() == 1u) {
+
+				auto &jsonItem = json[key];
+				std::visit([&jsonItem, &data](auto T:: *const ptrv)
+					{
+						jsonItem = data.*ptrv;
+					}, ptrList.front());
+			}
+			else {
+				for (const auto &member : ptrList) {
+
+					auto &jsonItem = json[key];
+					std::visit([&jsonItem, &data](auto T:: *const ptrv)
+						{
+							jsonItem.push_back(data.*ptrv);
+						}, member);
+
+				}
+			}
+
+		}
+	}
+
+	template<SoLib::IsNotPointer T>
+	inline void FromJson(const nlohmann::json &json, T &data) {
+		const auto &jsonPair = GetJsonPair<T>();
+
+		for (const auto &[key, ptrList] : jsonPair) {
+			if (ptrList.size() == 1u) {
+
+				const auto &jsonItem = json.at(key);
+				std::visit([&jsonItem, &data](auto T:: *const ptrv)
+					{
+						data.*ptrv = jsonItem;
+					}, ptrList.front());
+
+			}
+			else {
+				uint32_t index = 0u;
+				for (const auto &member : ptrList) {
+					const auto &jsonItem = json.at(key).at(index);
+					std::visit([&jsonItem, &data](auto T:: *const ptrv)
+						{
+							data.*ptrv = jsonItem;
+						}, member);
+					index++;
+				}
+			}
+		}
+	}
+
+}
+
+#pragma endregion
