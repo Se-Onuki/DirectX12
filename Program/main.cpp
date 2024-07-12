4c0ca44529d40e62c250490053bf94348443d779
S-Onuki <k022g0020@g.neec.ac.jp>
2024-07-12 23:33:24 +0900

    最初から読み込んだ状態にする

--- Program/main.cpp
+++ Program/main.cpp
@@ -123,7 +123,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	SceneManager *const sceneManager = SceneManager::GetInstance();
 	sceneManager->StaticInit();
 	sceneManager->Init();
-	sceneManager->ChangeScene("TitleScene", 0.f);
+	sceneManager->ChangeScene("GameScene", 0.f);
 
 	// ウィンドウのxボタンが押されるまでループ
 	while (true) {
