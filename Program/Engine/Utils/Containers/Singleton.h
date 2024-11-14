631544f90ce21bf0ff31642b86a7e487ebb54108
S-Onuki <k022g0020@g.neec.ac.jp>
2024-11-14 19:34:33 +0900

    シングルトンクラスのスレッドセーフの方法の変更

--- Program/Engine/Utils/Containers/Singleton.h
+++ Program/Engine/Utils/Containers/Singleton.h
@@ -2,11 +2,27 @@
 #include "../SoLib/SoLib_Traits.h"
 #include <memory>
 #include <mutex>
+#include <typeindex>
+
+/// シングルトンの記述のマクロ
+#define ConstructSingleton(Name) Name() = default;\
+Name(const Name &) = delete;\
+Name &operator= (const Name &) = delete;\
+~Name() = default;\
+friend SoLib::Singleton<Name>;
 
 namespace SoLib {
-
+	/// @brief シングルトンの基底クラス
+	class ISingleton {
+	public:
+		virtual void FinalizeSelf() {};
+	};
+	/// @brief シングルトンのスタック
+	class SingletonStacker;
+	/// @brief CRTPによるシングルトンクラス
+	/// @tparam T 定義するクラス
 	template <SoLib::IsRealType T>
-	class Singleton {
+	class Singleton : public ISingleton {
 		Singleton(const Singleton &) = delete;
 		Singleton &operator=(const Singleton &) = delete;
 
@@ -26,30 +42,41 @@ namespace SoLib {
 
 		static void Finalize();
 
+		void FinalizeSelf() override { Singleton::Finalize(); }
+
 	protected:
-
+		// インスタンスのポインタ
 		static std::unique_ptr<T, SingletonDeleter> instance_;
-		static std::mutex mutex_;
+		// 一度だけ実行されるフラグ
+		static std::once_flag onceFlag_;
 
+	private:
+
+		// 生成関数
+		static void Generate();
+
+	};
+
+	/// @brief シングルトンのスタック
+	class SingletonStacker : public Singleton<SingletonStacker> {
+		ConstructSingleton(SingletonStacker)
+	public:
+		// インスタンスの配列
+		std::list<std::unique_ptr<SoLib::ISingleton>> instanceList_;
 	};
 
 	template<SoLib::IsRealType T>
 	std::unique_ptr<T, typename Singleton<T>::SingletonDeleter> Singleton<T>::instance_ = nullptr;
 
 	template<SoLib::IsRealType T>
-	std::mutex Singleton<T>::mutex_;
+	std::once_flag Singleton<T>::onceFlag_;
 
 	template<SoLib::IsRealType T>
 	inline T *const Singleton<T>::GetInstance() {
-		{
-			// マルチスレッドのロックを掛ける
-			std::lock_guard<std::mutex> lock(mutex_);
-			// 実体がない場合
-			if (not instance_) {
-				// 生成
-				instance_.reset(new T);
-			}
-		}
+		// https://af-e.net/cpp-singleton-thread-safe
+
+		// 一度だけ生成関数を呼び出す
+		std::call_once(onceFlag_, Generate);
 		// ポインタを返す
 		return instance_.get();
 	}
@@ -57,6 +84,12 @@ namespace SoLib {
 	template<SoLib::IsRealType T>
 	inline void Singleton<T>::Finalize() {
 		instance_.reset();
+	}
+
+	template<SoLib::IsRealType T>
+	inline void Singleton<T>::Generate()
+	{
+		instance_.reset(new T);
 	}
 
 }
