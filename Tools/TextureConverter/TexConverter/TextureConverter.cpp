f1e5c3bbbc59e2274d83a9c07533e01690c0487d
S-Onuki <k022g0020@g.neec.ac.jp>
2024-11-23 01:33:19 +0900

    動作確認｡テクスチャの変換完了

--- Tools/TextureConverter/TexConverter/TextureConverter.cpp
+++ Tools/TextureConverter/TexConverter/TextureConverter.cpp
@@ -1,21 +1,89 @@
 #include "TextureConverter.h"
+
+#include <algorithm>
+#include <cassert>
 #include "../StringConverter/StringConverter.h"
 
-void SoLib::TextureConverter::ConvertTextureWIC2DDS(const char *const file_path)
-{
-	// テクスチャファイルを読み込む
-	WICTextureLoad(file_path);
+namespace SoLib {
+	void TextureConverter::ConvertTextureWIC2DDS(const char *const file_path)
+	{
+		// テクスチャファイルを読み込む
+		LoadWICTexture(file_path);
 
-	// DDS形式に変換して書き出す
+		// DDS形式に変換して書き出す
+		SaveDDSTexture();
 
-}
+	}
 
-void SoLib::TextureConverter::WICTextureLoad(const char *const file_path)
-{
-	// ファイルパスをワイド文字列に変換
-	const std::wstring wFile_Path = ConvertString(file_path);
+	void TextureConverter::LoadWICTexture(const char *const file_path)
+	{
+		HRESULT hr = S_FALSE;
 
-	// テクスチャ読み込み
+		// ファイルパスをワイド文字列に変換
+		const std::wstring wFile_Path = ConvertString(file_path);
+
+		// テクスチャファイルの名前を分解
+		textureFileName_ = SeparateFilePath(wFile_Path);
+
+		// テクスチャ読み込み
+		hr = DirectX::LoadFromWICFile(wFile_Path.c_str(), DirectX::WIC_FLAGS::WIC_FLAGS_NONE, &metaData_, scratchImage_);
+		assert(SUCCEEDED(hr));
 
 
+	}
+
+	void TextureConverter::SaveDDSTexture()
+	{
+		HRESULT hr = S_FALSE;
+
+		// 読み込んだデータをSRGBとして扱う
+		metaData_.format = DirectX::MakeSRGB(metaData_.format);
+		// 出力ファイルを設定する
+		const std::wstring file_path = textureFileName_.GetFilePath(L".dds");
+		// DDSファイルを書き出し
+		hr = DirectX::SaveToDDSFile(scratchImage_.GetImages(), scratchImage_.GetImageCount(), metaData_, DirectX::DDS_FLAGS::DDS_FLAGS_NONE, file_path.c_str());
+
+	}
+
+	TextureConverter::TextureFileName TextureConverter::SeparateFilePath(const std::wstring_view &file_path) const
+	{
+		// 返すデータ
+		TextureFileName result;
+		// ファイル名の転送
+		std::wstring filePath{ file_path };
+		// 文字を変換
+		std::replace(filePath.begin(), filePath.end(), '/', '\\');
+
+		size_t pos1{};
+		std::wstring exceptExt;
+		// 区切り文字 '.'が出てくる最初の部分を探す
+		pos1 = filePath.rfind('.');
+		// 検索でヒットしたら
+		if (pos1 != std::wstring::npos) {
+			// 区切り文字の後ろを拡張子として読む
+			result.fileExt_ = filePath.substr(pos1 + 1, filePath.size() - pos1 - 1);
+			// それ以前を抜き出す
+			exceptExt = filePath.substr(0, pos1);
+		}
+		// 拡張子がなかった場合
+		else {
+			result.fileExt_ = L"";
+			exceptExt = filePath;
+		}
+		// 区切り文字'\\'が出てくる一番最後の部分を探す
+		pos1 = exceptExt.rfind('\\');
+		if (pos1 != std::wstring::npos) {
+			// 区切り文字の前までをディレクトリパスとして保存
+			result.directoryPath_ = exceptExt.substr(0, pos1 + 1);
+			// 区切り文字以降をファイル名として保存
+			result.fileName_ = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+		}
+		// 生のフォルダだった場合
+		else {
+			result.directoryPath_ = L"";
+			result.fileName_ = exceptExt;
+		}
+
+		return result;
+	}
 }
--- Tools/TextureConverter/TexConverter/TextureConverter.h
+++ Tools/TextureConverter/TexConverter/TextureConverter.h
@@ -1,9 +1,32 @@
 #pragma once
+#include "DirectXTex/DirectXTex.h"
+#include <string>
 
 namespace SoLib {
 
 	class TextureConverter {
 	public:
+
+		struct TextureFileName {
+			// ディレクトリパス
+			std::wstring directoryPath_;
+			// ファイル名
+			std::wstring fileName_;
+			// ファイル拡張子
+			std::wstring fileExt_;
+
+			/// @brief ファイル名を構築する
+			/// @return 結合したファイル名
+			std::wstring GetFilePath() const {
+				return directoryPath_ + fileName_ + fileExt_;
+			}
+			/// @brief ファイル名を構築する
+			/// @param fileExt 拡張子
+			/// @return 結合したファイル名
+			std::wstring GetFilePath(const std::wstring_view &fileExt) const {
+				return directoryPath_ + fileName_ + fileExt.data();
+			}
+		};
 		TextureConverter() = default;
 
 		/// @brief テクスチャをWICからDDSに変換する
@@ -15,9 +38,23 @@ namespace SoLib {
 
 		/// @brief テクスチャのロード
 		/// @param file_path 
-		void WICTextureLoad(const char *const file_path);
+		void LoadWICTexture(const char *const file_path);
 
-		void Convert();
+		/// @brief DDSファイルへ変換して保存する
+		void SaveDDSTexture();
+
+		/// @brief パスの分解
+		/// @param file_path ファイルパス
+		/// @return ファイルパス
+		TextureFileName SeparateFilePath(const std::wstring_view &file_path) const;
+
+		// テクスチャファイルの名前
+		TextureFileName textureFileName_;
+
+		// 画像データ
+		DirectX::TexMetadata metaData_;
+		// 画像イメージのコンテナ
+		DirectX::ScratchImage scratchImage_;
 
 	};
 
